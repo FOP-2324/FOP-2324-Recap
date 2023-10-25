c0e818e5ec2f892bc2985f6b908384430948a3b4
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2023-10-25 11:06:44 +0200

    Add r01 code

--- src/main/java/r01/Main.java
+++ src/main/java/r01/Main.java
@@ -1,7 +1,38 @@
 package r01;
 
+import fopbot.Robot;
+import fopbot.World;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        /*
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i);
+        }
+        */
+        /*
+        int a = 42;
+        // a = a + 5;
+        a += 5;
+        System.out.println(a);
+        */
+        /*
+        World.setSize(8, 8);
+        World.setDelay(200);
+        World.setVisible(true);
+
+        Robot robby = new Robot(0, 0);
+        robby.move();
+        */
+        /*
+        moveRobotByAmount(robby, 5);
+        */
+    }
+
+    public static void moveRobotByAmount(Robot robot, int steps) {
+        for (int i = 0; i < steps; i++) {
+            robot.move();
+        }
     }
 }
